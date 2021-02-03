541026bec976f231de654feb4474ad79e3fecddf
frillic <69489498+crowe828@users.noreply.github.com>
2021-02-03 12:16:06 -0500

    created footer component and added styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import "../App.css";
 import Cards from "../components/Cards";
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Cards />
+      <Footer />
     </>
   );
 }
